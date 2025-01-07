fd8c8656e7756aa2496417d14089538494b5e756
Ericsun1999 <appp123456789@gmail.com>
2025-01-07 13:14:24 +0800

    Update Figure1&2.R

--- Code/Figure1&2.R
+++ Code/Figure1&2.R
@@ -8,23 +8,23 @@ library(ggplot2)
 
 #Annual counts of temperature records in the REACHES dataset from 1368 to 1911, Figure 1(a)
 
-jpeg("./anntemp.png",width=4,height=2.5 , res = 300, units = "in")
+#jpeg("./anntemp.png",width=4,height=2.5 , res = 300, units = "in")
     print(
        ggplot(temperature, aes(x=year)) +
     geom_histogram(binwidth = 1,color ="red",fill="red") +
     theme(text = element_text(size=15))
     )
-    dev.off()
+#    dev.off()
 
 #Frequency of each temperature category in the REACHES dataset, Figure 1(b)
 
-jpeg("./temphisto.png",width=4,height=2.5 , res = 300, units = "in")
+#jpeg("./temphisto.png",width=4,height=2.5 , res = 300, units = "in")
     print(
        ggplot(temperature,aes(x=level)) +
     geom_histogram(bins=9) +
     theme(text = element_text(size=15))
     )
-    dev.off()
+ #   dev.off()
 
 # Take mode for events at duplicated locations and rearrange the data by year
 
@@ -36,17 +36,7 @@ temp2 <-temperature%>%
 
 # A map of temperature event levels, Figure2
 
-  ggplot(temp2,aes(long,lat)) +
-    borders(database="world",xlim=c(95,126),ylim=c(19,45),fill=NA,colour="grey30") +
-    geom_point(aes(colour=level),cex=1) +
-    coord_map(xlim=c(98,124.5),ylim=c(19,42.5)) + 
-    scale_colour_gradientn(colours=rev(brewer.pal(n=9,name='RdBu')),
-                             limits=c(-2,2),na.value="transparent",
-                             guide="colourbar") +
-    theme(text=element_text(size=14),legend.position="right",legend.title=element_blank(),legend.key.height=unit(3.5,"cm"),legend.spacing.y = unit(-3,"cm"))
-
-  
-  jpeg("~/Downloads/recordstem1.png",width=6,height=5.5, res=300, units = "in")
+  #jpeg("~/Downloads/recordstem1.png",width=6,height=5.5, res=300, units = "in")
       print(
       ggplot(temp2,aes(long,lat)) +
     borders(database="world",xlim=c(95,126),ylim=c(19,45),fill=NA,colour="grey30") +
@@ -57,9 +47,5 @@ temp2 <-temperature%>%
                              guide="colourbar") +
     theme(text=element_text(size=17),legend.position="right",legend.title=element_blank(),legend.key.height=unit(3.5,"cm"),legend.spacing.y = unit(-3,"cm"))
       )
-    dev.off() 
-
-
-
-    
+   # dev.off() 
 
