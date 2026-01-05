cf8a2864ecec0909e37b04f4aeb8fce5d89708a8
Ericsun1999 <appp123456789@gmail.com>
2026-01-05 14:42:54 -0600

    Update Get_tempe_all_data.R

--- Code/Get_tempe_all_data.R
+++ Code/Get_tempe_all_data.R
@@ -1,55 +1,5 @@
-
-library(readxl)
-temperature<- read_excel("./temperature index value.v1.xlsx",col_type = c("skip","skip","numeric","numeric","skip","skip","skip","skip","skip","numeric","numeric","skip","skip"))
-colnames(temperature)<-c("level","year","long","lat")
-
-# Take mode for events at duplicated locations and rearrange the data by year
-
-library(dplyr)
-  
-temp2 <-temperature%>% 
-    group_by(year,long,lat) %>%  
-    summarise(level = mean(level))
-
-
-# Compute empirical variograms based on all the data at various years
-  library(sp)
-  library(spacetime)
-  library(zoo)
-  library(gstat)
-
-  y2<- temp2[temp2$year>100 & temp2$year<=2000,]
-  coord <- unique(round(as.data.frame(cbind(long=y2$long,lat=y2$lat)),4))
-  coordinates(coord) <- ~ long + lat
-  proj4string(coord) <- CRS('+proj=longlat +datum=WGS84')
-  year <- sort(unique(y2$year))
-  YM <- as.yearmon(year)
-  temp23 <- as.data.frame(round(as.matrix(y2),4))
-  ind <- matrix(NA,nrow(temp23),2)
-  ind[,2] <- match(temp23$year,year)
-  ind[,1] <- match(temp23$long*10^7+temp23$lat,coord@coords[,1]*10^7+coord@coords[,2])
-  stsdf2 <- STSDF(sp=coord,time=YM,data=y2,index=ind)
-  vario.stsdf2 <- variogramST(level~1,data=stsdf2,tlags=0,width=10,na.omit=T)
-
-# Semi-variogram model fitting
-  coordinates(y2) <- ~ long + lat
-  proj4string(y2) <- CRS('+proj=longlat +datum=WGS84')
-  vario2 <- variogram(level~1,data=y2,width=10)
-  ind <- match(as.numeric(rownames(vario.stsdf2))-1,rownames(vario2))
-  ind2 <- rep(F,nrow(vario2))
-  ind2[ind] <- T
-  vario2 <- vario2[ind2,]
-  if(sum(vario.stsdf2$dist==0)>0) vario2[,1:3] <- vario.stsdf2[ind[-1],1:3] else vario2[,1:3] <- vario.stsdf2[ind,1:3]
-  
-  vario.fit2 <- fit.variogram(vario2,vgm(model="Exp",nugget=NA),fit.kappa=T,fit.method=7)
-
-#Bias corrected
-
-vario.fit2$psill <- c(0.146, 0.739)
-vario.fit2$range <- c(0, 299.099)
-
-
 # Kriging on 0.5x0.5 grid points
+
 year.start <- range(temp2$year)[1]
 year.end <- range(temp2$year)[2]
 year.all <- year.start:year.end
@@ -70,7 +20,6 @@ loc <- expand.grid(long = seq(98.25, 124.25, by = 0.5), lat = seq(18.25, 42.25, 
 coordinates(loc) <- ~ long + lat
 proj4string(loc) <- CRS('+proj=longlat +datum=WGS84')
 
-#predicted
 
 library(MASS)
 library(mvtnorm)
