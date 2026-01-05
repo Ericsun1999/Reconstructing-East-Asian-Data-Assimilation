4f9e6d8c2071f6665a35aa454ab9814a450b7ab7
Ericsun1999 <appp123456789@gmail.com>
2026-01-04 21:37:42 -0600

    Update and rename Figure1&2.R to Figure2.R

--- Code/Figure2.R
+++ Code/Figure2.R
@@ -5,20 +5,11 @@ temperature<- read_excel("./temperature index value.v1.xlsx",col_type = c("skip"
 colnames(temperature)<-c("level","year","long","lat")
 
 library(ggplot2)
+library(dplyr)
+  
+#Figure2b, Empirical frequencies of the temperature categories
 
-#Figure 1(a), Annual counts of temperature records in the REACHES dataset from 1368 to 1911
-
-#jpeg("./anntemp.png",width=4,height=2.5 , res = 300, units = "in")
-    print(
-       ggplot(temperature, aes(x=year)) +
-    geom_histogram(binwidth = 1,color ="red",fill="red") +
-    theme(text = element_text(size=15))
-    )
-#    dev.off()
-
-#Figure 1(b), Frequency of each temperature category in the REACHES dataset
-
-#jpeg("./temphisto.png",width=4,height=2.5 , res = 300, units = "in")
+#jpeg("./Figure2(b).png",width=4,height=2.5 , res = 300, units = "in")
     print(
        ggplot(temperature,aes(x=level)) +
     geom_histogram(bins=9) +
@@ -26,15 +17,33 @@ library(ggplot2)
     )
  #   dev.off()
 
+year_count <- temperature %>%
+  count(year)
+  
+#Figure2a, Annual counts of temperature records in the REACHES dataset
+
+#jpeg("./Figure2(a).png",width=4,height=2.5 , res = 300, units = "in")
+    print(
+       ggplot(year_count, aes(x = year, y = n)) +
+  geom_col(fill = "red", color = "red") +
+  geom_smooth(method = "loess", span = 0.3, color = "black", linewidth = 1, se = FALSE) +
+  theme(text = element_text(size = 15)) +
+  labs(y = "count")
+    )
+#    dev.off()
+
+
+
 # Take mode for events at duplicated locations and rearrange the data by year
 
 library(dplyr)
 
 temp2 <-temperature%>% 
-    group_by(year,long,lat) %>%  
-    summarise(level = mean(level))
+    group_by(year,long,lat) 
 
-plot_originREACHES_map <- function(data, text_size = 17, legend_height_cm = 3.5, legend_spacing_cm = -3) {
+library(RColorBrewer)
+
+plot_originREACHES_map <- function(data, text_size = 15, legend_height_cm = 2.5, legend_spacing_cm = 30) {
   ggplot(data, aes(long, lat)) +
     borders(database = "world", xlim = c(95, 126), ylim = c(19, 45), fill = NA, colour = "grey30") +
     geom_point(aes(colour = level), cex = 1) +
@@ -47,18 +56,18 @@ plot_originREACHES_map <- function(data, text_size = 17, legend_height_cm = 3.5,
     ) +
     theme(
       text = element_text(size = text_size),
-      legend.position = "right",
+      legend.position = c(1.12,0.61),
       legend.title = element_blank(),
       legend.key.height = unit(legend_height_cm, "cm"),
-      legend.spacing.y = unit(legend_spacing_cm, "cm")
+      #legend.key.spacing.y = unit(legend_spacing_cm, "cm")
     )
 }
 
-#Figure2, A map of temperature event levels
-  
-#  jpeg("~/Downloads/recordstem1.png",width=6,height=5.5, res=300, units = "in")
+#Figure2c, Spatial distribution of temperature records across East Asia
+
+  #jpeg("./Figure2(c).png",width=5,height=4, res=300, units = "in")
       print(
       plot_originREACHES_map(temp2)
       )
-#    dev.off() 
+    #dev.off() 
 
