3a17e3db17b2c27b3f4bd72e3a1430942b55f0e7
Ericsun1999 <appp123456789@gmail.com>
2026-01-05 16:04:22 -0600

    Update Figure6e.R

--- Code/Figure6e.R
+++ Code/Figure6e.R
@@ -20,3 +20,276 @@ loc <- expand.grid(long = seq(97.5, 125, by = 2.5), lat = seq(18, 42.63158, by =
 coordinates(loc) <- ~ long + lat
 proj4string(loc) <- CRS('+proj=longlat +datum=WGS84')
 
+y2 <- var.fit2$y2
+
+library(RColorBrewer)
+library(ggplot2)
+library(MASS)
+library(mvtnorm)
+
+sigmay = sqrt(vario.fit2$psill2)
+sigmae = sqrt(vario.fit2$psill1)
+
+sigma = sqrt(vario.fit2$psill1 + vario.fit2$psill2)
+alpha = vario.fit2$range/100
+cuts = c(-Inf, -1.5, -0.5, 0.5, Inf)
+vals = c(-2, -1, 0, 1)
+
+mu_z <- -pnorm(-1.5, mean = 0, sd = sqrt(sigmay + sigmae))
+
+cov_Y <- function(s1, s2) {
+  (sigmay)^2 * exp(-sqrt(sum((s1-s2)^2))/alpha)
+}
+
+calc_c_Y <- function(s0, locations) {
+  n <- nrow(locations)
+  c_Y <- numeric(n)
+  for (i in 1:n) {
+    si <- locations[i, ]
+    c_Y[i] <- cov_Y(s0, si)
+  }
+  return(c_Y)
+}
+
+# E[h(Z*)]
+Ez_discrete <- function(sigma, cuts, vals) {
+  stopifnot(length(vals) == length(cuts) - 1)
+  a <- head(cuts, -1) / sigma
+  b <- tail(cuts, -1) / sigma
+  probs <- pnorm(b) - pnorm(a)
+  sum(vals * probs)
+}
+
+
+# E[Z* h(Z*)]
+EZstar_h <- function(sigma, cuts, vals) {
+  stopifnot(length(vals) == length(cuts) - 1)
+  a <- head(cuts, -1) / sigma
+  b <- tail(cuts, -1) / sigma
+  part <- sigma * (dnorm(a) - dnorm(b))
+  sum(vals * part)
+}
+
+# Cov(Z_i, Z_j)
+cov_Z_pair <- function(rho, sigma, cuts, vals, Eh = NULL) {
+  if (is.null(Eh)) Eh <- Ez_discrete(sigma, cuts, vals)
+  K <- length(vals)
+  a_std <- head(cuts, -1) / sigma
+  b_std <- tail(cuts, -1) / sigma
+
+  Sigma2 <- matrix(c(1, rho, rho, 1), 2, 2)
+  Eh2 <- 0
+  for (k in seq_len(K)) {
+    for (l in seq_len(K)) {
+      lower <- c(a_std[k], a_std[l])
+      upper <- c(b_std[k], b_std[l])
+      pij <- as.numeric(pmvnorm(lower = lower, upper = upper, mean = c(0,0), sigma = Sigma2))
+      Eh2 <- Eh2 + vals[k] * vals[l] * pij
+    }
+  }
+  Eh2 - Eh^2
+}
+
+cZY_vector <- function(s_coords, s0, sigma_Y2, sigma_E2, alpha,
+                       cuts = c(-Inf, -1.5, -0.5, 0.5, Inf),
+                       vals = c(-2, -1, 0, 1)) {
+  sigma2 <- sigma_Y2 + sigma_E2
+  sigma  <- sqrt(sigma2)
+  Ezstar_h <- EZstar_h(sigma, cuts, vals)
+  dists <- sqrt(rowSums((s_coords - matrix(s0, nrow(s_coords), ncol(s_coords), byrow=TRUE))^2))
+  covYY  <- sigma_Y2 * exp(-dists / alpha)
+  (covYY / sigma2) * Ezstar_h
+}
+
+cZY_matrix <- function(s_coords, s0_mat, sigma_Y2, sigma_E2, alpha,
+                       cuts, vals) {
+  if (is.null(dim(s0_mat))) s0_mat <- matrix(s0_mat, nrow = 1)
+  n <- nrow(s_coords); m <- nrow(s0_mat)
+  sigma2 <- sigma_Y2 + sigma_E2
+  sigma  <- sqrt(sigma2)
+  # E[Z* h(Z*)]
+  EZh <- EZstar_h(sigma, cuts, vals)
+  dists <- as.matrix(dist(rbind(s_coords, s0_mat)))[1:n, (n+1):(n+m)]
+  covYY <- sigma_Y2 * exp(-dists / alpha)          # n × m
+  (covYY / sigma2) * EZh                           # n × m
+}
+
+SigmaZ_matrix <- function(s_coords, sigma_Y2, sigma_E2, alpha,
+                          cuts = c(-Inf, -1.5, -0.5, 0.5, Inf),
+                          vals = c(-2, -1, 0, 1)) {
+  n <- nrow(s_coords)
+  sigma2 <- sigma_Y2 + sigma_E2
+  sigma  <- sqrt(sigma2)
+  Eh <- Ez_discrete(sigma, cuts, vals)
+  
+  # d_ij = ||s_i - s_j||
+  dmat <- as.matrix(dist(s_coords, method = "euclidean", upper = TRUE, diag = TRUE))
+  rho  <- (sigma_Y2 * exp(-dmat / alpha)) / sigma2
+  diag(rho) <- 1  
+
+  Sig <- matrix(NA_real_, n, n)
+  for (i in 1:n) {
+    for (j in i:n) {
+      cij <- cov_Z_pair(rho[i,j], sigma, cuts, vals, Eh = Eh)
+      Sig[i,j] <- cij
+      if (j != i) Sig[j,i] <- cij
+    }
+  }
+  Sig
+}
+
+y_pred <- function(s_coords, s0,
+                   z_obs = NULL,
+                   sigma_Y2, sigma_E2, alpha,
+                   cuts = c(-Inf, -1.5, -0.5, 0.5, Inf),
+                   vals = c(-2, -1, 0, 1),
+                   tol = 1e-2,
+                   return = c("mean", "var")) {
+  return <- unique(return)
+
+  if (!is.null(z_obs)) {
+    stopifnot(length(z_obs) == nrow(s_coords))
+  }
+
+  c_zy   <- cZY_vector(s_coords, s0, sigma_Y2, sigma_E2, alpha, cuts, vals)
+  SigmaZ <- SigmaZ_matrix(s_coords, sigma_Y2, sigma_E2, alpha, cuts, vals)
+  SigmaZ <- (SigmaZ + t(SigmaZ)) / 2  
+
+  out <- list()
+  
+  # --- mean: c' Σ^{-1} (z - Ez) ---
+  if ("mean" %in% return) {
+    sigma2 <- sigma_Y2 + sigma_E2
+    sigma  <- sqrt(sigma2)
+    Ez <- Ez_discrete(sigma, cuts, vals)
+
+    rhs <- z_obs - Ez
+    w_mean <- solve(SigmaZ, rhs, tol = tol)
+    out$mean <- drop(crossprod(c_zy, w_mean))
+  }
+
+  # --- var: sigma_Y2 - c' Σ^{-1} c ---
+  if ("var" %in% return) {
+    w_var <- solve(SigmaZ, c_zy, tol = tol)
+    var_pred <- sigma_Y2 - drop(crossprod(c_zy, w_var))
+    out$var <- max(var_pred, 0)
+  }
+  if (length(out) == 1) return(out[[1]])
+  out
+}
+
+
+  for(i in 1:length(year2)) {
+    temp14 <- y2[y2$year==year2[i],]
+    temp15 <- rep(0, length(loc@coords[,1]))
+    temp16 <- rep(0, length(loc@coords[,1]))
+    
+    locations<- temp14@coords
+    
+    
+    c_Y <- matrix(0, nrow = length(loc@coords[,1]), ncol = length(temp14$level))
+    Mat1<- matrix(mu_z, nrow = 1, ncol = length(temp14$level))
+    
+    for (j in 1:length(loc@coords[,1])) {
+      
+      s0 <- as.numeric(loc@coords[j,])
+      c_Y[j,] <- calc_c_Y(s0, locations)
+      res <- y_pred(locations, s0,  as.matrix(temp14$level), sigmay, sigmae, alpha, 
+                    return = c("mean","var"))
+      temp15[j]<- res$mean
+      temp16[j] <- res$var
+    }
+    arr.pred[,,i] <- matrix(temp15,53,49)
+    arr.std[,,i] <- matrix(temp16,53,49)
+    temp.dat1 <- cbind(as.data.frame(loc@coords)[,1:2],mu=c(arr.pred[,,i]), std = c(arr.std[,,i]))
+
+  }
+
+  for(i in 1:length(year2)) {
+    temp14 <- y2[y2$year==year2[i],]
+    temp15 <- rep(0, length(loc@coords[,1]))
+    temp16 <- rep(0, length(loc@coords[,1]))
+    
+    locations<- temp14@coords
+    
+    Sigma_zz <- calc_Sigma_zz(locations)
+    invsigma_zz<- solve(Sigma_zz)
+    c_Y <- matrix(0, nrow = length(loc@coords[,1]), ncol = length(temp14$level))
+    Mat1<- matrix(mu_z, nrow = 1, ncol = length(temp14$level))
+    
+    for (j in 1:length(loc@coords[,1])) {
+      
+      s0 <- as.numeric(loc@coords[j,])
+      c_Y[j,] <- calc_c_Y(s0, locations)
+      temp15[j]<- t(as.matrix(c_Y[j,])) %*% as.matrix(invsigma_zz) %*% (as.matrix(temp14$level) - t(Mat1) )
+      temp16[j] <- cov_Y(s0,s0) - t(c_Y[j,]) %*% as.matrix(invsigma_zz) %*% (c_Y[j,])
+    }
+    arr.pred[,,i] <- matrix(temp15,12,14)
+    arr.std[,,i] <- matrix(temp16,12,14)
+    
+    if(i==1) sk.all <- sk.all1 <- as.data.frame(loc@coords)
+    sk.all <- cbind(sk.all,c(arr.pred[,,i]))
+    sk.all1 <- cbind(sk.all1,c(arr.std[,,i]))
+    
+  }
+  
+  names(sk.all)[-c(1,2)] <- year2
+  names(sk.all1)[-c(1,2)] <- year2
+  
+tempe_all <- sk.all
+
+library(sf)
+library(rnaturalearth)
+library(dplyr)
+library(stringr)
+
+
+world <- ne_countries(scale = "medium", returnclass = "sf")
+targets <- world %>%
+  filter(str_detect(admin, regex("China|Taiwan|Hong Kong|Macao|Macau", ignore_case = TRUE)))
+
+china_tw_hk_mo <- st_union(targets) 
+china_tw_hk_mo <- st_as_sf(data.frame(name = "China+Taiwan+HK+MO"), geometry = china_tw_hk_mo)
+st_crs(china_tw_hk_mo) <- 4326
+
+coast_km <- 80
+china_tw_hk_mo_valid <- st_make_valid(china_tw_hk_mo)
+poly_m <- st_transform(china_tw_hk_mo_valid, 3857)
+poly_m_buf <- st_buffer(poly_m, dist = coast_km * 1000)
+china_tw_hk_mo_buf <- st_transform(poly_m_buf, 4326)
+pts_sf <- st_as_sf(tempe_all, coords = c("long", "lat"), crs = 4326)
+
+tempe_all$in_china_tw_hk_mo_coast <- st_within(
+  pts_sf,
+  china_tw_hk_mo_buf,
+  sparse = FALSE
+)[,1]
+tempe_all
+
+tempe_all1 <- tempe_all[(tempe_all$in_china_tw_hk_mo_coast == "TRUE"), -c(527)]
+
+nu_reaches <- sk.all1
+nu_reaches1 <- nu_reaches[(tempe_all$in_china_tw_hk_mo_coast == "TRUE"),]
+
+#Load LME data
+Data1 <- read.csv("./a1.csv", row.names=1)
+Data2 <- read.csv("./a2.csv", row.names=1)
+Data3 <- read.csv("./a3.csv", row.names=1)
+Data4 <- read.csv("./a4.csv", row.names=1)
+Data5 <- read.csv("./a5.csv", row.names=1)
+Data6 <- read.csv("./a6.csv", row.names=1)
+Data7 <- read.csv("./a7.csv", row.names=1)
+Data8 <- read.csv("./a8.csv", row.names=1)
+Data9 <- read.csv("./a9.csv", row.names=1)
+Data10 <- read.csv("./a10.csv", row.names=1)
+Data11 <- read.csv("./a11.csv", row.names=1)
+Data12 <- read.csv("./a12.csv", row.names=1)
+Data13 <- read.csv("./a13.csv", row.names=1)
+
+
+
+
+
+
+
+  
