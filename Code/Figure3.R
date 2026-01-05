e2f90826bf81a02515f83191a804e7cb4d5187a6
Ericsun1999 <appp123456789@gmail.com>
2026-01-04 22:14:24 -0600

    Update Figure3.R

--- Code/Figure3.R
+++ Code/Figure3.R
@@ -3,55 +3,31 @@
 #Data2 Shanghai
 #Data3 Beijing
 
-Data1 <- read.csv("./a1.csv", row.names=1)
-Data2 <- read.csv("./a2.csv", row.names=1)
-Data3 <- read.csv("./a3.csv", row.names=1)
+Data1 <- read.csv("./d1.csv", row.names=1)
+Data2 <- read.csv("./d2.csv", row.names=1)
+Data3 <- read.csv("./d3.csv", row.names=1)
 
+library(ggplot2)
 
 #Beijing
 Data <- Data3 #Plot Shanghai or hong kong just change it to Data2 or Data1
 
-haa <- matrix(0, nrow = 13, ncol = 600)
-
-for (k in 1:13) {
-  for (i in 1:600) {
-    idx <- (-9):(2) + 12 * i
-    haa[k, i] <- rowMeans(Data[k, idx] - 273)
-  }
-}
-
-haave <- colMeans(haa)
-haave1 <- as.vector(haa)
-
-
-#ggplot LME 
-
-library(ggplot2)
-
-dfp<-data.frame(
-  data1=haa[1,],
-  data2=haa[2,],
-  data3=haa[3,],
-  data4=haa[4,],
-  data5=haa[5,],
-  data6=haa[6,],
-  data7=haa[7,],
-  data8=haa[8,],
-  data9=haa[9,],
-  data10=haa[10,],
-  data11=haa[11,],
-  data12=haa[12,],
-  data13=haa[13,],
-  dataave=haave,
-  Year=c(1350:1949)
+haa<- (DData[,-c(1,2)]) - 273
+mean_row <- colMeans(haa, na.rm = TRUE)
+dfp <- data.frame(t(haa))
+dfp$dataave <- as.numeric(mean_row) 
+dfp$Year <- as.numeric(1350:1949)
+colnames(dfp) <- c(
+  paste0("data", 1:13),
+  "dataave",
+  "Year"
 )
+    
+#Figure3, Yearly temperature time series data from 13 LME simulations
 
-#Figure3(a), Yearly temperature time series data from 13 LME simulations 
-#(This show Bejing, for Shanghai and Hong Kong, edit line 73-75)
-
-#jpeg("~/Downloads/lmetime63.png",width=6,height=3, res=300, units = "in")
+#jpeg("./Figure3.png",width=6,height=3, res=300, units = "in")
       print(
-      ggplot(data=dfp[19:562,], aes(x=Year)) +
+      ggplot(data=dfp, aes(x=Year)) +
   geom_line(aes(y=data1,col="data1"))+
   geom_line(aes(y=data2,col="data2"))+
   geom_line(aes(y=data3,col="data3"))+
@@ -70,9 +46,9 @@ dfp<-data.frame(
   ylab("temperature")+
   xlab("year")+
   xlim(1368,1911)+
-  ylim(9,13.5)+
-# ylim(14,20) + #Shanghai
-# ylim(21.5,25) + #Hong Kong
+#  ylim(9,13.5)+ #Beijing
+# ylim(13,18) + #Shanghai
+# ylim(20,24) + #Hong Kong
   theme(text=element_text(size=12), plot.title = element_text(hjust = 0.5), legend.title = element_text(),legend.position = "none")
       )
- #  dev.off()
+#   dev.off()
