1c220790bc3a4aab874832c49f83326907d2aea1
Ericsun1999 <appp123456789@gmail.com>
2026-07-24 21:56:26 -0500

    Update Figure7e.R

--- Code/Figure7e.R
+++ Code/Figure7e.R
@@ -1,5 +1,25 @@
 here::i_am("Code/Figure7e.R")
 
+calibration_file <- here::here(
+  "Output",
+  "Intermediate",
+  "calibration_parameters.rds"
+)
+
+if (!file.exists(calibration_file)) {
+  stop(
+    "Required calibration file was not found: ",
+    calibration_file,
+    "\nRun Code/prepare_calibration.R first."
+  )
+}
+
+calibration_results <- readRDS(calibration_file)
+
+temp2 <- calibration_results$temp2
+var.fit2 <- calibration_results$var_fit2
+vario.fit2 <- calibration_results$vario_fit2
+
 ###It should be executed after running Figure4.R
 
 # Kriging on 2.5x1.89 grid points
